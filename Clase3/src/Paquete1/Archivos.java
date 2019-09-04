d917a3e19cdd07ec444e932723f41a8cdd4391d8
camilaiglesiasguerrero <camila.ig08@gmail.com>
2019-09-04 00:17:41 -0300

    escritura y lectura stream - Copiar archivo - Copiar carpeta completa -> falta ver como copiar estructura de tablas

--- Clase3/src/Paquete1/Archivos.java
+++ Clase3/src/Paquete1/Archivos.java
@@ -1,8 +1,12 @@
 package Paquete1;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -35,13 +39,71 @@ public class Archivos {
 			File f = new File("archivo.txt");// genero el archivo en ram
 			FileReader fr = new FileReader(f);// qué se va a hacer con el archivo
 			BufferedReader br = new BufferedReader(fr);// si el archivo no existe fisicamente arroja IOexception
-			String s = br.readLine();//busca saltos de linea.
-			while(s!= null){
+			String s = br.readLine();// busca saltos de linea.
+			while (s != null) {
 				System.out.println(s);
-				s = br.readLine();//busca saltos de linea. En cada linea, borro el buffer
+				s = br.readLine();// busca saltos de linea. En cada linea, borro el buffer
 			}
+			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+	public void leerYCopiarStreamer() {
+		try {
+			//Inicio leer Streamer
+			File fo = new File("C:/imgPA.jpg"); // file origen
+			FileInputStream fis = new FileInputStream(fo); // de qué forma vamos a hacer el input
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			//Fin leer - Inicio copiar
+			File fd = new File("nuevo.jpg");// file destino
+			FileOutputStream fos = new FileOutputStream(fd);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			//fin copiar
+			int cant = 1024;// cantidad de bytes q voy a leer
+			int lei = 0;//cantidad de bytes que logr+o leer
+			byte[] buff = new byte[cant];// Los array en Java son objetso, necesito instanciarlos
+			while((lei = bis.read(buff, 0, cant))>-1) {// parametros: donde va a guardar, desde hasta de cant de bytes q voy a leer,
+													//retorno: cuántos bytes leyó. si no tiene mas nada, devuelve -1
+				bos.write(buff,0,lei);
+			}
+			bos.close();
+			bis.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void leerYCopiarCarpetaCompleta() {
+		try {
+			File pathOrigen = new File("C:\\Camera Roll");//ruta completa
+			File[] todos = pathOrigen.listFiles();
+			String name;
+			File fd = new File("//Imagenes");
+			fd.mkdir();
+			for(File f1 : todos) {
+				name = f1.getName();
+				FileInputStream fis = new FileInputStream(f1);
+				BufferedInputStream bis = new BufferedInputStream(fis);
+				File fDestino = new File(name);
+				FileOutputStream fos = new FileOutputStream(fDestino);
+				BufferedOutputStream bos = new BufferedOutputStream(fos);
+				int cant = 1024;
+				int lei = 0;
+				byte[] buff = new byte[cant];
+				while((lei = bis.read(buff, 0, cant))>-1) {
+					bos.write(buff,0,lei);
+				}
+				bos.close();
+				bis.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
 }
--- Clase3/src/Paquete1/Principal.java
+++ Clase3/src/Paquete1/Principal.java
@@ -4,9 +4,10 @@ package Paquete1;
 public class Principal {
 	public static void main(String[] args) {
 		Archivos a = new Archivos();
-		a.escribirArchivo();
-		
-		a.leerArchivo();
+		//a.escribirArchivo();
+		//a.leerArchivo();
+		//a.leerYCopiarStreamer();
+		a.leerYCopiarCarpetaCompleta();
 	}
 
 }
